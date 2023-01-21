eb13eb534dcbb346c4405621a11b6e362849c853
Andentu <severalmaz97@gmail.com>
2023-01-21 18:00:17 +0300

    Добавьте файлы проекта.

---
+++ Final task 4_5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Final_task_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string surname, string login, int LoginCount, int age, bool pet, string[] colors) User;
+            Console.WriteLine("Введите имя");
+            User.name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            User.surname = Console.ReadLine();
+            Console.WriteLine("Введите логин");
+            User.login = Console.ReadLine();
+
+        }
+    }
+}
